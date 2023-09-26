2f213939bd53b36c014fefd1f8f7eec227d62135
James-Kyprianou <129155769+James-Kyprianou@users.noreply.github.com>
2023-09-26 15:26:44 +0930

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,50 @@
+window.addEventListener('load', function () {
+  // Show Breakfast section initially and set the title.
+  showMenu('Breakfast', 'Breakfast');
+});
+let currentSection = 'Breakfast';
 
+function showMenu(sectionId, title) {
+  // Remove the 'active' class from all navigation items.
+  const navItems = document.querySelectorAll('.sub-menu a');
+  navItems.forEach(item => item.classList.remove('active'));
+
+  // Add the active class to the clicked navigation item.
+  const clickedNavItem = document.querySelector(`[onclick="showMenu('${sectionId}', '${title}')"]`);
+  if (clickedNavItem) {
+    clickedNavItem.classList.add('active');
+  }
+
+  // Hide the current section.
+  const current = document.getElementById(currentSection);
+  if (current) {
+    current.style.display = 'none';
+  }
+
+  // Show the selected section.
+  const selectedSection = document.getElementById(sectionId);
+  if (selectedSection) {
+    selectedSection.style.display = 'block';
+    currentSection = sectionId; // Update the current section.
+  }
+
+  // Set the title.
+  document.getElementById('menuTitle').textContent = title;
+
+  // Scroll to the selected section smoothly
+  scrollToSection(sectionId);
+}
+
+function scrollToSection(sectionId) {
+  const targetSection = document.getElementById(sectionId);
+  if (targetSection) {
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+}
+/*
 window.addEventListener('load', function () {
   // Show Breakfast section initially and set the title.
   showMenu('Breakfast', 'Breakfast');
@@ -27,7 +73,7 @@ function showMenu(sectionId, title) {
   // Set the title.
   document.getElementById('menuTitle').textContent = title;
 }
-
+*/
 /*
 // Menu Page //
 		
