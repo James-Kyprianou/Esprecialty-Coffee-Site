a2d478119608b24a802fb273aba4891a9ed15ec4
James-Kyprianou <129155769+James-Kyprianou@users.noreply.github.com>
2023-09-26 16:08:51 +0930

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     const fragment = window.location.hash.substring(1); // Get the id without the '#'.
-    
     if (fragment) {
-        // If there's a fragment, show the corresponding section.
+        // If correct id, show the corresponding section.
         showMenu(fragment, fragment);
 
-        // Set the active class for the corresponding navigation item.
+        // Set the active class for the corresponding nav item.
         setActiveNavItem(fragment);
     } else {
-        // If there's no fragment, show the first section of the menu. 
+        // If there's no correct id, show the first section of the menu. 
         showMenu('Breakfast', 'Breakfast');
 
-        // Set the active class for the default navigation item.
+        // Set the active class for the default nav item. (Breakfast)
         setActiveNavItem('Breakfast');
     }
 });
-
 let currentSection = 'Breakfast';
-
 function showMenu(sectionId, title) {
     // Hide the current section.
     const current = document.getElementById(currentSection);
     if (current) {
         current.style.display = 'none';
     }
-
     // Show the selected section.
     const selectedSection = document.getElementById(sectionId);
     if (selectedSection) {
         selectedSection.style.display = 'block';
         currentSection = sectionId; // Update the current section.
     }
-
     // Set the title.
     document.getElementById('menuTitle').textContent = title;
 }
-
 function setActiveNavItem(sectionId) {
     const navItems = document.querySelectorAll('.nav-link');
     navItems.forEach(item => {
@@ -46,68 +40,3 @@ function setActiveNavItem(sectionId) {
         }
     });
 }
-/*
-window.addEventListener('load', function () {
-  // Show Breakfast section initially and set the title.
-  showMenu('Breakfast', 'Breakfast');
-});
-let currentSection = 'Breakfast';
-function showMenu(sectionId, title) {
-  // Remove the 'active' class from all navigation items.
-  const navItems = document.querySelectorAll('.sub-menu a');
-  navItems.forEach(item => item.classList.remove('active'));
-  // Add the active class to the clicked navigation item.
-  const clickedNavItem = document.querySelector(`[onclick="showMenu('${sectionId}', '${title}')"]`);
-  if (clickedNavItem) {
-    clickedNavItem.classList.add('active');
-  }
-  // Hide the current section.
-  const current = document.getElementById(currentSection);
-  if (current) {
-    current.style.display = 'none';
-  }
-  // Show the selected section.
-  const selectedSection = document.getElementById(sectionId);
-  if (selectedSection) {
-    selectedSection.style.display = 'block';
-    currentSection = sectionId; // Update the current section.
-  }
-  // Set the title.
-  document.getElementById('menuTitle').textContent = title;
-}
-*/
-/*
-// Menu Page //
-		
-document.addEventListener('DOMContentLoaded', function () {
-    const fragment = window.location.hash.substring(1); // Get the id without the '#'.
-    
-    if (fragment) {
-        // If correct id, show the corresponding section.
-        showMenu(fragment, fragment);
-    } else {
-        // If there's no correct id, show the first section of the menu. 
-        showMenu('Breakfast', 'Breakfast');
-    }
-});
-
-let currentSection = 'Breakfast';
-
-function showMenu(sectionId, title) {
-    // Hide the current section.
-    const current = document.getElementById(currentSection);
-    if (current) {
-        current.style.display = 'none';
-    }
-
-    // Show the selected section.
-    const selectedSection = document.getElementById(sectionId);
-    if (selectedSection) {
-        selectedSection.style.display = 'block';
-        currentSection = sectionId; // Update the current section.
-    }
-
-    // Set the title.
-    document.getElementById('menuTitle').textContent = title;
-}
-*/
