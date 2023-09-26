00757ad2026583a95e2fa19f0dcd0822501471d3
James-Kyprianou <129155769+James-Kyprianou@users.noreply.github.com>
2023-09-26 15:08:31 +0930

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 window.addEventListener('load', function () {
   // Show Breakfast section initially and set the title.
   showMenu('Breakfast', 'Breakfast');
@@ -25,4 +26,39 @@ function showMenu(sectionId, title) {
   }
   // Set the title.
   document.getElementById('menuTitle').textContent = title;
+}
+*/
+
+// Menu Page //
+		
+document.addEventListener('DOMContentLoaded', function () {
+    const fragment = window.location.hash.substring(1); // Get the id without the '#'.
+    
+    if (fragment) {
+        // If correct id, show the corresponding section.
+        showMenu(fragment, fragment);
+    } else {
+        // If there's no correct id, show the first section of the menu. 
+        showMenu('Breakfast', 'Breakfast');
+    }
+});
+
+let currentSection = 'Breakfast';
+
+function showMenu(sectionId, title) {
+    // Hide the current section.
+    const current = document.getElementById(currentSection);
+    if (current) {
+        current.style.display = 'none';
+    }
+
+    // Show the selected section.
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = 'block';
+        currentSection = sectionId; // Update the current section.
+    }
+
+    // Set the title.
+    document.getElementById('menuTitle').textContent = title;
 }
